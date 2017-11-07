ae6ea74a9b75d71f0a16cdb28b39186e6ac80c9e
Hyojin Kim <cobety0305@gmail.com>
2017-11-07 12:52:06 +0000

    first commit

---
+++ 1_Introduction/introduction.rb
@@ -0,0 +1,26 @@
+puts "I love espresso".length
+puts "Eric".reverse
+
+puts "eric".upcase
+down = "eric".upcase
+puts down.downcase
+
+# comment
+
+# multi line commets
+=begin
+I'm a comments!
+iyyeufiqfjdkfjkdj good!
+=end
+
+# Naming Conventions
+name = "Eric"
+age = 10
+
+# Math
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+# Strings and String Methods
+# "".methods 
---
+++ README.md
@@ -0,0 +1,11 @@
+# Learn Ruby 
+## Table of Contents
+---
+## 1.Introduction to Ruby
+- Introduction to Ruby
+- Putting the Form in Formatter
+
+
+## 2.Control Flow in Ruby
+- Control Flow in Ruby
+- Thith Meanth War!
