535aefa50d2af3fd37d7bb67bc0f6d8ebcc0e7a6
Hyojin Kim <cobety0305@gmail.com>
2017-11-08 23:29:20 +0000

    Learn about Hash

--- 4_Arrays_and_Hashes/data_structures.rb
+++ 4_Arrays_and_Hashes/data_structures.rb
@@ -37,3 +37,67 @@ pets = Hash.new
 pets["Stevie"] = "cat"
 pets["Mike"] = "dog"
 
+# Iterating over arrays
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each {|language| puts language}
+
+# Iterating over Multidimensional Arrays
+s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+s.each { |sub_array| sub_array.each {|element| puts element }}
+
+
+# Iterating over Hashes
+restaurant_menu = {
+  "noodles" => 4,
+  "soup" => 3,
+  "salad" => 2
+}
+
+restaurant_menu.each do |item, price|
+  puts "#{item}: #{price}"
+end
+
+
+secret_identities.each do |item, actor|
+  puts "#{item}: #{actor}"
+end
+
+
+secret_identities = {
+  "The Batman" => "Bruce Wayne",
+  "Superman" => "Clark Kent",
+  "Wonder Woman" => "Diana Prince",
+  "Freakazoid" => "Dexter Douglas"
+}
+
+
+# Multidimentional Arrays
+my_array = [["Korea", "Korean"],  ["Japan", "Japanese"], ["America", "English"]]
+
+
+# Create Hahes
+sounds = Hash.new
+sounds["dog"] = "woof"
+sounds["cat"] = "meow"
+
+puts sounds
+
+my_hash = Hash.new
+my_hash["Korea"] = "Korean"
+my_hash["Japan"] = "Japanese"
+puts my_hash
+
+
+#  Iterating Over a Hash
+# pick up just value
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each do |x, y|
+  puts "#{y}"
+end
